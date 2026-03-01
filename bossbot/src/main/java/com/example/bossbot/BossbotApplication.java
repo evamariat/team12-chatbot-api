9bb64c6421a0081132dac8dfdba5edd8ebc46c78
Kat <katlin.kalde@wenture.io>
2026-03-01 17:53:42 +0200

    Initial commit

---
+++ bossbot/src/main/java/com/example/bossbot/BossbotApplication.java
@@ -0,0 +1,13 @@
+package com.example.bossbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BossbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BossbotApplication.class, args);
+	}
+
+}
